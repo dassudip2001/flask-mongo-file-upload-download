056d17db0c23b1219c78881b584be265a1c19059
Sudip Das <sudip5428@gmail.com>
2022-10-01 23:11:47 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template,redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("base.html")
+
+
+
+    
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=5000)    
